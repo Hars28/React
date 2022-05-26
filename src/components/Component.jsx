261e0eb7822b8f1ad294e3e0a4cca936cea723ea
Hars28 <harsh.singh9055@gmail.com>
2022-05-26 20:17:36 +0530

    added first file

---
+++ src/components/Component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+        <ul>
+        <h1>Mobile Operating System</h1>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>Iphone</li>
+        <li>Windows Phone</li>
+        </ul>
+        <ul>
+        <h1>Mobile Manufacturers</h1>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Component
